0ebb2a4ef4b7d38b116c82878e5f8522f2c1073a
duyduong65 <tg.bluesky66@gmail.com>
2019-09-04 21:58:08 +0700

    add use_While-Ex_1

---
+++ Use_While/Ex_1.js
@@ -0,0 +1,10 @@
+
+let num = Number(prompt("Enter a numbers: "));
+let total = 0;
+while (num != (-1)) {
+    num = Number(prompt("Enter a numbers: "));
+    alert(num);
+    total = num + total;
+}
+alert(total);
+
