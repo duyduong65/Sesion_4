248202d9f08fe577eb30d6faaf4a364edb70139c
duyduong65 <tg.bluesky66@gmail.com>
2019-09-06 16:44:18 +0700

    so sánh ex_7 lần _1

--- Luyện Tập vòng lặp_1/ex_1-5.js
+++ Luyện Tập vòng lặp_1/ex_1-5.js
@@ -72,3 +72,18 @@ function findTotal() {
     }
     document.write(total);
 }
+
+function PrintFizzBuzz() {
+
+    for (let i = 1; i <= 50; i++) {
+        if (i % 15 == 0) {
+            i= "FizzBuzz";
+        } else if (i % 3 == 0) {
+            i = "Fizz";
+        } else if (i % 5 == 0) {
+            i = "Buzz";
+
+        }
+        document.write(i + "  ");
+    }
+}
