01df0d35af6de7ff2fe86b1e1be4fe02fd98144b
duyduong65 <tg.bluesky66@gmail.com>
2019-09-06 12:22:34 +0700

    final ex_2

--- Luyện Tập vòng lặp_1/ex_1-4.js
+++ Luyện Tập vòng lặp_1/ex_1-4.js
@@ -1,5 +1,25 @@
 function showNumbers() {
-    for (let i = 1; i<100;i++) {
+    for (let i = 1; i < 100; i++) {
     }
     alert("Done");
+}
+
+function convertTemplate() {
+    let inputTemplate = prompt("nhập vào nhiệt độ");
+    while (inputTemplate > 100) {
+        alert("Hãy giảm nhiệt độ");
+        break;
+    }
+    while (inputTemplate < 0) {
+        alert("Hãy tăng nhiệt độ");
+        break;
+    }
+}
+
+function showFibonacci() {
+    let n = 0;
+    for (let i = 0; i < 10; ) {
+        n += i;
+        document.write(n + "  ");
+    }
 }
