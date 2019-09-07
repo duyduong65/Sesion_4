61f554adcc8309f558770d7b3ec524ab07048982
duyduong65 <tg.bluesky66@gmail.com>
2019-09-07 20:39:47 +0700

    final ex1

--- Luyện tập vòng lặp _2/Example_full.js
+++ Luyện tập vòng lặp _2/Example_full.js
@@ -1,1 +1,11 @@
-
+function printFinonaci() {
+    let numbers_1 = 1;
+    let numbers_2 = 0;
+    let result = 1;
+    for (let i = 1; i <= 20; i++) {
+        result = numbers_1 + numbers_2;
+        numbers_1 = numbers_2;
+        numbers_2 = result;
+        document.write(result + "  ");
+    }
+}
